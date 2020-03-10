5cc44abbfddf90d1b3042d4758aa1a314b44b7b0
Breno Peretta <peretta.breno@gmail.com>
2020-03-09 23:55:47 -0300

    Ajuste na action e reducer do chooseIngredient

--- src/Store/actions/ChooseIngredients.js
+++ src/Store/actions/ChooseIngredients.js
@@ -1,17 +1,61 @@
+import store from '../store';
+
 export function chooseOption(value, option) {
-    return {
-        type: 'CHOOSE_OPTION',
-        value, 
-        option
+    const state = store.getState().chooseIngredient;
+
+    if(state.length > 0) {
+        for (let index = 0; index < state.length; index++) {
+            if (state[index].value === value) {
+                return {
+                    type: 'CHANGE_OPTION',
+                    value, 
+                    option,
+                    index
+                }
+            }                  
+        }
+        return {
+            type: 'ADD_OPTION',
+            value, 
+            option
+        }
+    } else {
+        return {
+            type: 'ADD_OPTION',
+            value, 
+            option
+        }
     }
 }
 
 export function chooseIngredient(id_request, value, option) {
-    return {
-        type: 'CHOOSE_INGREDIENT',
-        id_request,
-        value,
-        option
+    const state = store.getState().chooseIngredient;
+
+    if(state.length > 0) {
+        for (let index = 0; index < state.length; index++) {
+            if (state[index].value === value && state[index].id_ingredient === id_request) {
+                return {
+                    type: 'REMOVE_INGREDIENT',
+                    id_request,
+                    value,
+                    option, 
+                    index
+                }
+            }                  
+        }
+        return {
+            type: 'CHOOSE_INGREDIENT',
+            id_request,
+            value,
+            option
+        }
+    } else {
+        return {
+            type: 'CHOOSE_INGREDIENT',
+            id_request,
+            value,
+            option
+        }
     }
 }
 
--- src/Store/reducers/ChooseIngredient.js
+++ src/Store/reducers/ChooseIngredient.js
@@ -1,50 +1,34 @@
 export default function chooseIngredient(state = [], action) {
     console.log(state)
     switch(action.type) {
-        case 'CHOOSE_OPTION': 
-        if(state.length > 0) {
-            for (let index = 0; index < state.length; index++) {
-                if (state[index].value === action.value) {
-                //Edição de um objeto no state 
-                   return [
-                       ...state.slice(0, index), 
-                        {...state[index], option: action.option},
-                        ...state.slice(index + 1)
-                    ]
-                }                  
+        case 'ADD_OPTION':  
+        return [
+            ...state, { 
+                value: action.value,
+                option: action.option 
             }
-            //Adição de um novo objeto caso não seja edição
-            return [...state, { value: action.value,option: action.option }]
-        } else {
-            return [
-                ...state, { 
-                    value: action.value,
-                    option: action.option 
-                }
-            ]
-        }
-            
-        case 'CHOOSE_INGREDIENT': 
-        if(state.length > 0) {
-            for (let index = 0; index < state.length; index++) {
-                if (state[index].value === action.value && state[index].id_ingredient === action.id_request) {
-                //Deleta um objeto no state 
-                   return [
-                       ...state.slice(0, index), 
-                    ]
-                }                  
+        ]
+
+        case 'CHANGE_OPTION':
+        return [
+            ...state.slice(0, action.index), 
+            {...state[action.index], option: action.option},
+            ...state.slice(action.index + 1)
+        ]
+
+        case 'CHOOSE_INGREDIENT':
+        return [
+            ...state, {
+                id_ingredient: action.id_request, 
+                value: action.value,
+                option: action.option 
             }
-            //Adição de um novo objeto caso não seja para deletar
-            return [...state, { id_ingredient: action.id_request, value: action.value,option: action.option }]
-        } else {
-            return [
-                ...state, {
-                    id_ingredient: action.id_request, 
-                    value: action.value,
-                    option: action.option 
-                }
-            ]
-        }
+        ]
+
+        case 'REMOVE_INGREDIENT':
+        return [
+            ...state.slice(0, action.index), 
+        ]
 
         default: return state;
     }
